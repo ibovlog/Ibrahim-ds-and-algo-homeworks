bbea0c7410c583f27976aa1066c253ff484c68bc
ibovlog <ibonesirov14@gmail.com>
2026-09-25 02:43:16 +0400

    Task 7 ni  etdim

--- src/first/tasks/task7.java
+++ src/first/tasks/task7.java
@@ -1,4 +1,21 @@
 package first.tasks;
-
+import java.util.Scanner ;
 public class task7 {
+    static void main() {
+        Scanner obj = new Scanner (System.in) ;
+        int a = obj.nextInt() ;
+        if (a> 0) {
+            System.out.print("Musbet") ;
+
+        }
+        else {
+            if ( a == 0 ){
+                System.out.print("Sifir");
+
+            }
+            else {
+                System.out.print("Menfi");
+            }
+        }
+    }
 }
