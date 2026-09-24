280351e7065520c083d08baa1b7ff3aa25c6ea96
ibovlog <ibonesirov14@gmail.com>
2026-09-25 01:45:53 +0400

    Task 3 etdim.

--- src/first/tasks/task3.java
+++ src/first/tasks/task3.java
@@ -1,4 +1,13 @@
 package first.tasks;
-
+import java.util.Scanner;
 public class task3 {
+    static void main() {
+
+
+        Scanner obj = new Scanner(System.in);
+        int Mebleg = obj.nextInt();
+        int Faiz = obj.nextInt();
+        int Cavab = (Mebleg * Faiz) / 100;
+        System.out.println(Cavab);
+    }
 }
