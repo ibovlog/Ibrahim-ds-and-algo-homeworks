bce8bf9d81210860bca36c6d6c17b4f3741773e1
ibovlog <ibonesirov14@gmail.com>
2026-09-25 00:59:48 +0400

    Task 2- ni etdim .

--- src/first/tasks/task2.java
+++ src/first/tasks/task2.java
@@ -1,5 +1,20 @@
 package first.tasks;
+import java.util.Scanner ;
+public class task2 {
+    public static void main() {
+        Scanner myobj  = new Scanner (System.in) ;
+        int a = myobj.nextInt();
+        int b = myobj.nextInt();
+        int Cem = a+b ;
+        int Ferq = a-b ;
+        int Hasil = a*b ;
+        int Bolme  = a/b ;
+        int Qaliq = a%b ;
+        System.out.println((Cem) + " "+ (Ferq)+ " " + (Hasil)+ " " +  (Bolme) +" " +  (Qaliq));
 
-public class task2
-{
+
+
+    }
+
+        
 }
