fb379fcd5c63c250aff4a559dd4a56c6a9a115a8
ibovlog <ibonesirov14@gmail.com>
2026-09-25 02:33:37 +0400

    Task 6 ni  etdim .

--- src/first/tasks/task6.java
+++ src/first/tasks/task6.java
@@ -1,4 +1,23 @@
 package first.tasks;
-
+import java.util.Scanner ;
 public class task6 {
+    static void main() {
+        Scanner obj = new Scanner (System.in);
+        int a = obj.nextInt();
+        int b = obj.nextInt() ;
+        int c = obj.nextInt() ;
+        if  (a>b && a>c) {{
+            System.out.print(a);
+        }}
+        else {
+            if (b>a && b>c) {
+                System.out.print(b);
+
+            }
+            else {
+                System.out.print(c) ;
+            }
+        }
+
+    }
 }
