59b8520c5bc9376e51a821abf257737fed71500e
ibovlog <ibonesirov14@gmail.com>
2026-09-25 02:50:15 +0400

    Task 8 i etdim .

--- src/first/tasks/task8.java
+++ src/first/tasks/task8.java
@@ -1,4 +1,20 @@
 package first.tasks;
-
+import java.util.Scanner ;
 public class task8 {
+    static void main() {
+        Scanner obj = new Scanner(System.in) ;
+        int Yas = obj.nextInt();
+        if (Yas>0 && Yas<12) {
+            System.out.print("usag") ;
+        }
+        else{
+            if (Yas>13 && Yas < 17) {
+                System.out.print("yeniyetme");
+            }
+            else {
+                System.out.print("yetkin");
+            }
+        }
+
+    }
 }
