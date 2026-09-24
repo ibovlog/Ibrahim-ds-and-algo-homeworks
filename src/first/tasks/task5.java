e9b8eb02abe4868314d0737679f5ebbce0f5f52d
ibovlog <ibonesirov14@gmail.com>
2026-09-25 02:21:29 +0400

    Task 5 i etdim .

--- src/first/tasks/task5.java
+++ src/first/tasks/task5.java
@@ -1,4 +1,14 @@
 package first.tasks;
-
+import java.util.Scanner ;
 public class task5 {
+    static void main() {
+        Scanner obj = new Scanner (System.in) ;
+        int a   = obj.nextInt();
+        if (a%2 == 0 ){
+            System.out.print("CUT");
+        }
+        else {
+            System.out.print("TEK");
+        }
+    }
 }
