798ab004bad8783e9718c6326885fa281d39796d
ibovlog <ibonesirov14@gmail.com>
2026-09-25 11:05:10 +0400

    TASK 12 NI ETDIM

--- src/first/tasks/task12.java
+++ src/first/tasks/task12.java
@@ -1,4 +1,24 @@
 package first.tasks;
+import java.util.Scanner;
 
 public class task12 {
+    static void main() {
+        Scanner obj = new Scanner(System.in) ;
+        int a = obj.nextInt() ;
+        switch (a) {
+            case 1 :
+                System.out.print("TOPLAMA");
+                break ;
+            case 2 :
+                System.out.print("CIXMA");
+                break ;
+            case 3 :
+                System.out.print("VURMA");
+                break ;
+            case 4 :
+                System.out.print("BOLME");
+                break ;
+        }
+    }
+
 }
