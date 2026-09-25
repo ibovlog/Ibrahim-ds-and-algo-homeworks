9d927cc92abfb72ee8278070e6fd0aac1ddce6c5
ibovlog <ibonesirov14@gmail.com>
2026-09-25 13:05:36 +0400

    Task 15 i etdim.

--- src/first/tasks/task15.java
+++ src/first/tasks/task15.java
@@ -1,4 +1,15 @@
 package first.tasks;
+import java.util.Scanner ;
 
 public class task15 {
+    static void main() {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        int f = 1 ;
+        for (int i = 1 ; i<= n ; i++){
+            f = f*i ;
+        }
+        System.out.print(f);
+
+    }
 }
