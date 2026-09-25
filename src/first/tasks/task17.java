8876180a7494719789551cd0e66d0cf7e0f29590
ibovlog <ibonesirov14@gmail.com>
2026-09-25 13:19:52 +0400

    Task 17 ni etdim .

--- src/first/tasks/task17.java
+++ src/first/tasks/task17.java
@@ -1,4 +1,14 @@
 package first.tasks;
+import java.util.Scanner;
 
 public class task17 {
+    static void main() {
+        int s = 0 ;
+
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        while (a!=0 ){
+        s+=1;  a = obj.nextInt();}
+    System.out.println(s);
+    }
 }
