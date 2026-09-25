4585a95d7c6f1c1dee892ac2e225f9d33346fffe
ibovlog <ibonesirov14@gmail.com>
2026-09-25 12:55:05 +0400

    Task 13 u etdim.

--- src/first/tasks/task13.java
+++ src/first/tasks/task13.java
@@ -1,4 +1,15 @@
 package first.tasks;
-
+import java.util.Scanner;
 public class task13 {
+    static void main() {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        int s = 0 ;
+        for (int i = 0 ; i<n; i++) {
+            s = s+ i;
+
+
+        }
+        System.out.print(s);
+    }
 }
