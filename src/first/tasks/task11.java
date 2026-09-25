a457fd9e82880019417e0c5d2dc80de8c4c1403c
ibovlog <ibonesirov14@gmail.com>
2026-09-25 10:57:23 +0400

    Task 11 i etdim.

--- src/first/tasks/task11.java
+++ src/first/tasks/task11.java
@@ -1,4 +1,47 @@
 package first.tasks;
-
+import java.util.Scanner;
 public class task11 {
+    static void main() {
+        Scanner obj = new Scanner(System.in);
+        int ay = obj.nextInt();
+        switch (ay){
+            case 1:
+                System.out.println("QIS") ;
+                break;
+            case 2:
+                System.out.println("QIS") ;
+                break;
+            case 3:
+                System.out.println("YAZ") ;
+                break;
+            case 4:
+                System.out.println("YAZ") ;
+                break;
+            case 5:
+                System.out.println("YAZ") ;
+                break;
+            case 6:
+                System.out.println("YAY") ;
+                break;
+            case 7:
+                System.out.println("YAY") ;
+                break;
+            case 8:
+                System.out.println("YAY") ;
+                break;
+            case 9:
+                System.out.println("PAYIZ") ;
+                break;
+            case 10:
+                System.out.println("PAYIZ") ;
+                break;
+            case 11:
+                System.out.println("PAYIZ") ;
+                break;
+            case 12:
+                System.out.println("QIS") ;
+                break;
+        }
+
+    }
 }
