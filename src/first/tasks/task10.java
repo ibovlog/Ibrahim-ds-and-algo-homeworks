8b51c5ba068d86c4741f1da36d5f67fd9e9f07c2
ibovlog <ibonesirov14@gmail.com>
2026-09-25 10:38:11 +0400

    Task 10 u etdim

--- src/first/tasks/task10.java
+++ src/first/tasks/task10.java
@@ -1,4 +1,35 @@
 package first.tasks;
-
+import java.util.Scanner ;
 public class task10 {
+    static void main() {
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        switch (a) {
+            case 1:
+                System.out.print("BIRINCI GUN");
+                break ;
+            case 2 :
+                System.out.print("IKINCI GUN");
+                break;
+            case 3:
+                System.out.print("UCHUNCU GUN");
+                break;
+            case 4 :
+                System.out.print("DORDUNCU GUN") ;
+                break;
+            case 5 :
+                System.out.print("BESINCI GUN") ;
+                break;
+            case 6 :
+                System.out.print("ALTINCI GUN");
+                break;
+            case 7 :
+                System.out.print("BAZAR GUNU") ;
+                break;
+
+
+
+
+        }
+    }
 }
