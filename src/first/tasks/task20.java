8f7622adf8a909760262fd79c033dde39f1608a2
ibovlog <ibonesirov14@gmail.com>
2026-09-25 13:39:52 +0400

    Task 20 de bitdii😊😊😊

--- src/first/tasks/task20.java
+++ src/first/tasks/task20.java
@@ -1,4 +1,17 @@
 package first.tasks;
+import java.util.Scanner ;
 
 public class task20 {
+    static void main() {
+        Scanner obj = new Scanner(System.in) ;
+        int a = obj.nextInt();
+        int s = 0 ;
+        while (a!=0){
+            s = s + a%10 ;
+            a= a/10 ;
+        }
+        System.out.print(s);
+    }
+
+
 }
