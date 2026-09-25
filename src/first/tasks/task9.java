d3f27f5815e8271f1645685070eda01239e1105a
ibovlog <ibonesirov14@gmail.com>
2026-09-25 10:30:23 +0400

    Task 9 u etdim

--- src/first/tasks/task9.java
+++ src/first/tasks/task9.java
@@ -1,4 +1,21 @@
 package first.tasks;
-
+import java.util.Scanner;
 public class task9 {
+    static void main() {
+        Scanner obj = new Scanner(System.in);
+        int Bal = obj.nextInt();
+        int Bal2 = Bal/ 10 ;
+        switch (Bal2) {
+            case 10 :
+                System.out.print("A");
+                break;
+
+            case  9 :
+                System.out.print("A");
+                break;
+        }
+
+
+    }
+
 }
