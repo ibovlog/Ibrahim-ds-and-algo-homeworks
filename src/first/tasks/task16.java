31903944a13201c096d9fd585db8210a92bee344
ibovlog <ibonesirov14@gmail.com>
2026-09-25 13:09:16 +0400

    Task 16 ni etdim.

--- src/first/tasks/task16.java
+++ src/first/tasks/task16.java
@@ -1,4 +1,12 @@
 package first.tasks;
+import java.util.Scanner ;
 
 public class task16 {
+    static void main() {
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        for(int i = 1 ; i<= 10 ; i++){
+            System.out.println(a*i);
+        }
+    }
 }
