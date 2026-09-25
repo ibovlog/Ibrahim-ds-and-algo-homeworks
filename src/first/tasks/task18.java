2f85d6c3738b966a7cbe09ab5697ed76abaea7a3
ibovlog <ibonesirov14@gmail.com>
2026-09-25 13:24:55 +0400

    Task 18i etdim.

--- src/first/tasks/task18.java
+++ src/first/tasks/task18.java
@@ -1,4 +1,17 @@
 package first.tasks;
+import java.util.Scanner ;
 
 public class task18 {
+    static void main() {
+        Scanner obj = new Scanner (System.in);
+        int a = obj.nextInt();
+        int s = 1;
+        while (a>10){
+            s+=1;
+            a=a/10;
+
+
+        }
+        System.out.println(s);
+    }
 }
