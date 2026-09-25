c477ae4737900f9ec219f1f12d556ed223ff0432
ibovlog <ibonesirov14@gmail.com>
2026-09-25 13:35:13 +0400

    Task 19 u  etdim .

--- src/first/tasks/task19.java
+++ src/first/tasks/task19.java
@@ -1,4 +1,16 @@
 package first.tasks;
+import java.util.Scanner;
 
 public class task19 {
+    static void main() {
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        int x = 0 ;
+        while (a!=0){
+            x = x*10+(a%10) ;
+            a = a/10;
+
+        }
+        System.out.print(x);
+    }
 }
